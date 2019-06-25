6e0c5a0d6d7552451e7e72ae0793e6ecce0c13e6
Ilja Leiko <leikoilja@gmail.com>
2019-06-25 09:01:58 +0200

    Updating the package version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-haystack',
-    version='2.1.1-dev',
+    version='2.1.1-dev-v2',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
